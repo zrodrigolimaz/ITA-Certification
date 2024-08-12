ecd173ced895b4e33b7539a4e7810d06f8eefd14
Your Name <zrodrigolimaz@gmail.com>
2024-08-12 07:37:08 -0300

    chore: standalone project

--- livrologia-app/src/app/app.component.spec.ts
+++ livrologia-app/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'book-app' title`, () => {
+  it(`should have as title 'livrologia-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-app');
+    expect(app.title).toEqual('livrologia-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, book-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, livrologia-app');
   });
 });
